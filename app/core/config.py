c50cb742a6225a92f186848f6e85604442f1c2ae
Arian Ghanooni <ghanooni.ari@gmail.com>
2026-06-03 09:44:44 +0330

    Create router of the Tasks

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
-
+import os
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = Field(..., env="SQLALCHEMY_DATABASE_URL")
+    SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./sqlite.db")
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore"
     )
-
 
 settings = Settings()
